d4aba59784d97dd7a2a6d97a4917666f069ea63a
FalguniMehrotra17 <93004424+FalguniMehrotra17@users.noreply.github.com>
2021-10-27 18:38:55 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package JAVA_PRACTICE;
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+		System.out.print("Welcome to JAVA Class");
+	}
+
+}
---
+++ IfelseLoop.java
@@ -0,0 +1,19 @@
+package JAVA_PRACTICE;
+
+public class IfelseLoop {
+	public static void main(String args[]) {
+		int a=10,b=20,c=30;
+		if (a>b && a>c) 
+			System.out.print("a is the greatest number" + "\t"+a);
+		
+		else if 
+			(b>a && b>c) 
+				System.out.print("b is the greatest number" + "\t"+b);
+			
+		
+		else 
+			System.out.print("c is the greatest number" + "\t"+c);
+		
+	}
+
+}
